c8b1fa9c71df39a152e8ad7d76810861f9149bf2
Danushka <dnadanushka>
2020-04-25 05:41:21 +0530

    Web app SignUp

--- my-app/src/Methods/authMethod.js
+++ my-app/src/Methods/authMethod.js
@@ -42,15 +42,44 @@ export async  function loginRequest(nic,pw,setloginStates,setUserData){
         })
 }
 
-export async  function registerRequest(infomation ,setloginStates){
-    const response =
-      await axios.get("https://dog.ceo/api/breeds/list/all",
-          { params: {name: 'bruno'}}
-      );
-    console.log(response)
-    setloginStates(response.data)
-
-    return response.data;
+export async  function registerRequest(name,nic,posi,phone,access,ds,gs,pw){
+  console.log(nic+' '+pw)
+  const qs = require('querystring');
+  const url = getIp()+'adminRegister';
+  const requestBody = {
+      name: name,
+      nic:nic,
+      position:posi,
+      primaryContact:phone,
+      DSID:ds,
+      GNID:gs,
+      ALID:access,
+      password:pw
+    }
+    
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }
+    
+    axios.post(url, qs.stringify(requestBody), config)
+      .then((result) => {
+        // Do somthing
+          console.log(result)
+          // if(result.data.PasswordHash != null){
+          //     localStorage.setItem('token', result.data);
+          //     console.log(result.data)
+          //     setUserData(result.data)
+          //     setloginStates(true)
+          // }else{
+          //     setloginStates(false);
+          // }
+      })
+      .catch((err) => {
+        console.log(err)
+        //setloginStates(false);
+      })
 }
 
 
